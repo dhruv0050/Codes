4d0de088aa9c2175004ed50938304b1e1e2388ae
Dhruv Sharma <149249311+dhruv0050@users.noreply.github.com>
2023-12-28 15:25:27 +0530

    Add files via upload

---
+++ code1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a;
+    long long b;
+    char c;
+    double d;
+    long double e;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cin>>d;
+    cin>>e;
+    ;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    cout<<fixed<<d<<endl;
+    cout<<fixed<<e<<endl;
+    return 0;
+}
