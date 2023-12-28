b76733442f343d3e822df008be917ac1b303b1fd
Dhruv Sharma <149249311+dhruv0050@users.noreply.github.com>
2023-12-28 15:27:18 +0530

    Update code1.cpp

--- code1.cpp
+++ code1.cpp
@@ -1,3 +1,10 @@
+//Input consists of the following space-separated values: int, long, char, float, and double, respectively.
+
+//Output Format
+
+//Print each element on a new line in the same order it was received as input. 
+//Note that the floating point value should be correct up to 3 decimal places and the double to 9 decimal places.
+
 #include <iostream>
 #include <cstdio>
 using namespace std;
