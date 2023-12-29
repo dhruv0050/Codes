43e36e5859275f23d99b8d790ae8104a49f7e6b8
Dhruv Sharma <149249311+dhruv0050@users.noreply.github.com>
2023-12-29 21:00:34 +0530

    Add files via upload

---
+++ code7.cpp
@@ -0,0 +1,12 @@
+//RECTANGLE AREA
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int len,bred;
+    cin>>len>>bred;
+    cout<<len<<" "<<bred<<endl;
+    cout<<len*bred;
+    return 0;
+}
