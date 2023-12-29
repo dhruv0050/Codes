90089b2b66c84e8182c7213b695283cd8d01217e
Dhruv Sharma <149249311+dhruv0050@users.noreply.github.com>
2023-12-29 20:41:13 +0530

    Add files via upload

---
+++ code5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int a , b;
+    cin>>a>>b;
+    int sum=a+b;
+    int diff=abs(a-b);
+    cout<<sum<<endl;
+    cout<<diff;  
+    return 0;
+}
