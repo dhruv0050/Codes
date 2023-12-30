6a9116774f4ea469fbb1b579ab16fdfb3e9a8e1b
Dhruv Sharma <149249311+dhruv0050@users.noreply.github.com>
2023-12-30 22:54:43 +0530

    Add files via upload

---
+++ code9.cpp
@@ -0,0 +1,28 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+struct student{
+    int age;
+    string first_name;
+    string last_name;
+    int standard;
+};
+int main(){
+    student s1;
+    cin>>s1.age;
+    cin>>s1.first_name;
+    cin>>s1.last_name;
+    cin>>s1.standard;
+    
+    cout<<s1.age<<" ";
+    cout<<s1.first_name<<" ";
+    cout<<s1.last_name<<" ";
+    cout<<s1.standard; 
+
+    return 0;
+}
